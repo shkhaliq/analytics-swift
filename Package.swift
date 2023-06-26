289dac54cbe2a5cc2e9eca81a5675515229be0cf
Haris Khaliq <7643859+shkhaliq@users.noreply.github.com>
2023-06-26 12:41:47 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Segment",
-            targets: ["Segment"]),
+            name: "InstacartSegment",
+            targets: ["InstacartSegment"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -30,7 +30,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Segment",
+            name: "InstacartSegment",
             dependencies: ["Sovran"]),
         .testTarget(
             name: "Segment-Tests",
